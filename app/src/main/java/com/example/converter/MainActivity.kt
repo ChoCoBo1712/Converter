6bd3a3e64fcfc6b4f3977b9e955bd76cb93dc653
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2020-09-28 17:26:11 +0300

    Bottom navigation added listener

--- app/src/main/java/com/example/converter/MainActivity.kt
+++ app/src/main/java/com/example/converter/MainActivity.kt
@@ -10,5 +10,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when(item.itemId) {
+            R.id.nav_length -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_weight -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.nav_volume -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            else -> {
+                false
+            }
+        }
     }
 }
