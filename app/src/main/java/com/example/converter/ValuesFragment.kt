86f238dfffa14536d9a95aabff7dae82bd560a57
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2020-10-27 21:54:43 +0300

    viewModel init

--- app/src/main/java/com/example/converter/ValuesFragment.kt
+++ app/src/main/java/com/example/converter/ValuesFragment.kt
@@ -5,14 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+
 
 class ValuesFragment : Fragment() {
+
+    private val viewModel: ViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_keyboard, container, false)
+
+
+        return inflater.inflate(R.layout.fragment_values, container, false)
     }
 
     companion object {
