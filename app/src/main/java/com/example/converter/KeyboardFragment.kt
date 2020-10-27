d88378869371a6238e0cf35453d367ff84ba960d
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2020-10-27 22:37:11 +0300

    KeyboardFragment.kt v1

--- app/src/main/java/com/example/converter/KeyboardFragment.kt
+++ app/src/main/java/com/example/converter/KeyboardFragment.kt
@@ -5,15 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.activityViewModels
 
 
-class KeyboardFragment : Fragment() {
+class KeyboardFragment : Fragment(), View.OnClickListener {
+
+    private val viewModel: ViewModel by activityViewModels()
+    private lateinit var service: Service
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_keyboard, container, false)
+
+        service = Service()
+
+        val view =  inflater.inflate(R.layout.fragment_keyboard, container, false)
+        view.findViewById<Button>(R.id.button0).setOnClickListener(this)
+        view.findViewById<Button>(R.id.button1).setOnClickListener(this)
+        view.findViewById<Button>(R.id.button2).setOnClickListener(this)
+        view.findViewById<Button>(R.id.button3).setOnClickListener(this)
+        view.findViewById<Button>(R.id.button4).setOnClickListener(this)
+        view.findViewById<Button>(R.id.button5).setOnClickListener(this)
+        view.findViewById<Button>(R.id.button6).setOnClickListener(this)
+        view.findViewById<Button>(R.id.button7).setOnClickListener(this)
+        view.findViewById<Button>(R.id.button8).setOnClickListener(this)
+        view.findViewById<Button>(R.id.button9).setOnClickListener(this)
+        view.findViewById<Button>(R.id.buttonDot).setOnClickListener(this)
+        view.findViewById<Button>(R.id.buttonDelete).setOnClickListener(this)
+        return view
+    }
+
+    override fun onClick(v: View?) {
+        val button = v as Button
+
+        when(button.id) {
+            R.id.buttonDelete -> {
+
+            }
+        }
     }
 
     companion object {
