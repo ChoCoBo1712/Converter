36f8cb60d856cc24c86381cce72f2f71edbb11d2
ChoCoBo1712 <ChoCoBo1712@users.noreply.github.com>
2020-11-01 15:18:29 +0300

    Added adapter

--- app/src/main/java/com/example/converter/MyViewModel.kt
+++ app/src/main/java/com/example/converter/MyViewModel.kt
@@ -7,5 +7,7 @@ class MyViewModel: ViewModel() {
     var value: MutableLiveData<String> = MutableLiveData("")
     var convertedValue: MutableLiveData<String> = MutableLiveData("")
     var dot: Boolean = false
+    var list: MutableLiveData<Int> = MutableLiveData(1)
+    var item: MutableLiveData<Int> = MutableLiveData(1)
 
 }
--- app/src/main/java/com/example/converter/ValuesFragment.kt
+++ app/src/main/java/com/example/converter/ValuesFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.Spinner
+import android.widget.SpinnerAdapter
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import kotlinx.android.synthetic.main.fragment_values.*
@@ -23,9 +26,16 @@ class ValuesFragment : Fragment() {
 
         val editTextLeft = view.findViewById<EditText>(R.id.editTextLeft)
         val editTextRight = view.findViewById<EditText>(R.id.editTextRight)
+        val spinnerLeft = view.findViewById<Spinner>(R.id.spinnerLeft)
+        val spinnerRight = view.findViewById<Spinner>(R.id.spinnerRight)
 
         viewModel.value.observe(viewLifecycleOwner, { newValue -> editTextLeft.setText(newValue) })
         viewModel.convertedValue.observe(viewLifecycleOwner, { newValue -> editTextRight.setText(newValue) })
+
+        val adapter = ArrayAdapter.createFromResource(requireActivity().applicationContext, R.array.length, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerLeft.adapter = adapter
+        spinnerRight.adapter = adapter
 
         return view
     }
